396a53a282446b02c7518a76852e52a7308884c5
Cybertek <vasilich85@gmail.com>
2021-02-23 22:21:06 -0800

    Small Update Login Page

--- src/test/java/com/libraryAutomation/pages/LoginPage.java
+++ src/test/java/com/libraryAutomation/pages/LoginPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 public class LoginPage extends BasePage{
 
     public LoginPage() {
-        url = "http://library1.cybertekschool.com/login.html";
+        url = ConfigurationReader.getProperty("url");
     }
 
     @FindBy(id="inputEmail")
